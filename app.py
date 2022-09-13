866032e06526359c9813dd6225e83299cb58253f
WiktorBK <wiktorsebbozek@gmail.com>
2022-09-13 14:31:10 +0200

    Added Postgresql

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask 
 from flask_restful import Api
 from security import authenticate, identity
@@ -7,7 +9,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL1', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'top_secret'
 api = Api(app)
