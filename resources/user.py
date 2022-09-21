4040ee5615bf7759af8ea9d9a237c8b700ca9137
WiktorBK <wiktorsebbozek@gmail.com>
2022-09-21 20:15:39 +0200

    fixed import errors

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import (
     create_access_token, 
     create_refresh_token, 
-    jwt_refresh_token_required, 
+    jwt_required, 
     get_jwt_identity)
 from models.user import UserModel
 import hmac
@@ -68,7 +68,7 @@ class UserList(Resource):
         return {'items': [user.json() for user in UserModel.find_all()]}
 
 class TokenRefresh(Resource):
-    @jwt_refresh_token_required
+    @jwt_required(refresh=True)
     def post(self):
         current_user = get_jwt_identity()
         new_token = create_access_token(identity=current_user, fresh=False)
